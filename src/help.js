7d8f782f48770cedd6c29818a71496ee9cae25ac
Aqlabriyan <78632978+aqlabriyan@users.noreply.github.com>
2021-10-13 07:01:38 +0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -12,32 +12,13 @@ usage : reply sticker\n
 command : *${prefix}tsticker* or *${prefix}tstiker*
 desc : convert text to sticker
 usage : *${prefix}tsticker text in here*\n
-> *Meme Commands* <
-command : *${prefix}meme*
-desc : random meme images [english]
-usage : just send the command\n
 > *Others Commands* <
-command : *${prefix}tp*
-desc : Text / Logo maker from textpro.me
-usage : *${prefix}tp [no] [text]*\nexample : *${prefix}tp 1 YourText*
-note : *${prefix}tp list* ( for show all theme )\n
-command : *${prefix}ep*
-desc : Text / Logo maker from ephoto360.com
-usage : *${prefix}ep [no] [text]*\nexample : *${prefix}ep 1 YourText*
-note : *${prefix}ep list* ( for show all theme )\n
 command : *${prefix}tahta*
 desc : Harta Tahta .....
 usage : *${prefix}tahta [text]*\nexample : *${prefix}tahta YourText*\n
 command : *${prefix}gtts*
 desc : convert text to speech/audio
 usage : *${prefix}gtts [cc] [text]*\nexample : *${prefix}gtts ja On2-chan*\n
-command : *${prefix}loli*
-desc : random loli images
-usage : just send the command\n
-command : *${prefix}nsfwloli*
-desc : random nsfw loli images
-usage : just send the command\n
-command : *${prefix}url2img*
 desc : take web screenshots
 usage : *${prefix}url2img [tipe] [url]*\n
 command : *${prefix}simi*
@@ -62,14 +43,6 @@ command : *${prefix}kick*
 desc : kick members from group
 usage : *${prefix}kick @tagmember*\n
 note : can only be used when the bot becomes admin, and the one who sends the command is admin!\n
-command : *${prefix}promote*
-desc : make the group member as group admin
-usage : *${prefix}promote @tagmember*\n
-note : can only be used when the bot becomes admin, and the one who sends the command is admin!\n
-command : *${prefix}demote*
-desc : make the group admin as group member
-usage : *${prefix}demote @tagmember*\n
-note : can only be used when the bot becomes admin, and the one who sends the command is admin!\n
 command : *${prefix}linkgroup*
 desc : take the group link
 usage : just send the command
@@ -82,10 +55,6 @@ command : *${prefix}tagall*
 desc : tags all group members including admins too
 usage : just send the command
 note : This command can be used if you are a group admin\n
-command : *${prefix}simih*
-desc : activate simi mode in the group
-usage : *${prefix}simih 1* to activate simi mode and *${prefix}simih 0* to deactivate simi mode
-note : This command can be used if you are a group admin\n`
 }
 
 exports.help = help
